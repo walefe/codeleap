dd74a0398fdc1e51f115b64f3c5052d1add28eba
Walefe Pereira de Jesus <walefedev@gmail.com>
2022-04-06 14:44:54 -0300

    chore: create page signup

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -3,7 +3,12 @@ import * as S from './styels'
 export const SignUp = () => {
   return (
     <S.Wrapper>
-      <h1>SignUp</h1>
+      <h1>Welcome to CodeLeap network!</h1>
+      <S.FormContainer>
+        <S.Text>Please enter your username</S.Text>
+        <S.Input placeholder="John Doe" />
+      </S.FormContainer>
+      <S.Button type="submit">Entrar</S.Button>
     </S.Wrapper>
   )
 }
--- src/pages/SignUp/styels.ts
+++ src/pages/SignUp/styels.ts
@@ -3,12 +3,47 @@ import styled from 'styled-components'
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
+  padding: 2.4rem;
 
   max-width: 50rem;
   width: 100%;
 
-  max-height: 2.2rem;
+  max-height: 22rem;
   height: 100%;
 
-  background-color: #cccc;
+  background-color: #fff;
+`
+
+export const FormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+
+  margin-top: 2.6rem;
+  height: auto;
+`
+
+export const Text = styled.p`
+  font-size: 1.6rem;
+`
+
+export const Input = styled.input`
+  border-radius: 0.4rem;
+  border: 0;
+  border: 2px solid #777777;
+  margin-top: 1rem;
+  padding: 0.4rem;
+
+  &::placeholder {
+    color: #777777;
+  }
+`
+export const Button = styled.button`
+  width: 11.1rem;
+  height: 3.3rem;
+  align-self: flex-end;
+  margin-top: 2.6rem;
+  background-color: black;
+  border: 0;
+  color: #fff;
+  font-weight: bold;
 `
