05a2c8b07558c053885a8a2980be59f854b4e66f
Walefe Pereira de Jesus <walefedev@gmail.com>
2022-04-10 22:13:35 -0300

    refactor: alter global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,7 +2,7 @@ import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
   :root {
-    --background: #f0f2f5;
+    --background: #e5e5e5;
   }
 
   * {
@@ -13,7 +13,7 @@ export default createGlobalStyle`
 
   html {
     font-size: 67.5%;
-    height: 100vh;
+    height: 100%;
 
     @media (max-width: 720px) {
       font-size: 87.5%; //14px
@@ -21,9 +21,21 @@ export default createGlobalStyle`
   }
 
   body {
+     height: auto;
     font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     background-color: var(--background);
     -webkit-font-smoothing: antialiased;
+  }
+
+    body, #root {
+    min-height: 100vh;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+  }
+
+  #root {
+    height: 100%;
   }
 
   button {
@@ -33,5 +45,25 @@ export default createGlobalStyle`
   [disabled] {
     opacity: 0.6;
     cursor: not-allowed;
+  }
+
+  .react-modal-overlay {
+    background: rgba(0, 0, 0, 0.5);
+
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .react-modal-content {
+    width: 100%;
+    max-width: 66.1rem;
+    background: #ffff;
   }
 `
