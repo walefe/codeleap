c793cf5da454dd5f34dd4ea29486ec476d9547a2
Walefe Pereira de Jesus <walefedev@gmail.com>
2022-04-10 22:11:56 -0300

    chore: config routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 
 import { SignUp } from '../pages/SignUp'
+import { Main } from '../pages/Main'
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<SignUp />} />
+      <Route path="/feed" element={<Main />} />
     </Routes>
   )
 }
