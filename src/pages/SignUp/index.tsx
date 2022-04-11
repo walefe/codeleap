cb1ca135985f9d3932f02fed23eadeda7634c747
Walefe Pereira de Jesus <walefedev@gmail.com>
2022-04-10 22:11:25 -0300

    chore: create page sign up

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,14 +1,45 @@
+import { FormEvent, useCallback, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import { useDispatch } from 'react-redux'
+import { createUser } from '../../redux/reducers'
+
 import * as S from './styels'
 
 export const SignUp = () => {
+  const [name, setName] = useState('')
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const submitForm = useCallback(
+    (event: FormEvent) => {
+      event.preventDefault()
+      dispatch(
+        createUser({
+          username: name
+        })
+      )
+      setName('')
+      navigate('/feed')
+    },
+    [name, dispatch, navigate]
+  )
+
   return (
     <S.Wrapper>
-      <h1>Welcome to CodeLeap network!</h1>
-      <S.FormContainer>
-        <S.Text>Please enter your username</S.Text>
-        <S.Input placeholder="John Doe" />
-      </S.FormContainer>
-      <S.Button type="submit">Entrar</S.Button>
+      <S.Content>
+        <h1>Welcome to CodeLeap network!</h1>
+        <S.FormContainer onSubmit={submitForm}>
+          <S.Text>Please enter your username</S.Text>
+          <S.Input
+            type="text"
+            placeholder="John Doe"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+          <S.Button type="submit">Entrar</S.Button>
+        </S.FormContainer>
+      </S.Content>
     </S.Wrapper>
   )
 }
--- src/pages/SignUp/styels.ts
+++ src/pages/SignUp/styels.ts
@@ -2,15 +2,22 @@ import styled from 'styled-components'
 
 export const Wrapper = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+`
+
+export const Content = styled.div`
+  display: flex;
   flex-direction: column;
   padding: 2.4rem;
   max-width: 50rem;
   width: 100%;
 
   max-height: 22rem;
-  height: 100%;
-
   background-color: #fff;
+  box-shadow: 0 0.5rem 2rem rgba(0, 0, 0, 0.08);
 `
 
 export const FormContainer = styled.form`
@@ -41,8 +48,9 @@ export const Button = styled.button`
   height: 3.3rem;
   align-self: flex-end;
   margin-top: 2.6rem;
-  background-color: #f56161;
+  background-color: #6933ff;
   border: 0;
+  border-radius: 0.4rem;
   color: #fff;
   font-weight: bold;
 
