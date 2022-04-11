7384ce713c3f8d26165309e3de0a5b1e92617253
Walefe Pereira de Jesus <walefedev@gmail.com>
2022-04-10 22:13:50 -0300

    refactor: alter layout

--- src/pages/_layouts/styles.ts
+++ src/pages/_layouts/styles.ts
@@ -1,11 +1,10 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  width: 100%;
-  max-width: 108rem;
-  height: 100vh;
-  margin: 0 auto;
   display: flex;
+  width: 100%;
+  max-width: 198rem;
+  margin: 0 auto;
   align-items: center;
   justify-content: center;
 `
