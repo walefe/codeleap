3555651049672c73bcbeb4b4180509deea86125b
Walefe Pereira de Jesus <walefedev@gmail.com>
2022-04-10 22:14:08 -0300

    chore: config redux provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
 import GlobalStyles from './styles/global'
 import { Router } from './routes'
 import { Layout } from './pages/_layouts'
 
+import { store } from './redux/store'
+
 function App() {
   return (
-    <Layout>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-      <GlobalStyles />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+        <GlobalStyles />
+      </Layout>
+    </Provider>
   )
 }
 
